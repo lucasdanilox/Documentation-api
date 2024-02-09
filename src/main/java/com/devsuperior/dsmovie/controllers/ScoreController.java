8a9632268532f00f2783c6ab5aa0c594048111a4
lucasdanilox <lukastecno@gmail.com>
2024-02-09 00:15:20 -0300

    Customizing API documentation

--- src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dsmovie.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,11 +18,23 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/scores")
+@Tag(name = "Score", description = "Controller Score Movie")
 public class ScoreController {
 	
 	@Autowired
 	private ScoreService service;
-	
+
+	@Operation(
+			description = "Endpoint for save score",
+			summary = "Save Score",
+			responses = {
+					@ApiResponse(description = "Ok", responseCode = "200"),
+					@ApiResponse(description = "Bad Request", responseCode = "400"),
+					@ApiResponse(description = "Unauthorized", responseCode = "401"),
+					@ApiResponse(description = "Forbidden", responseCode = "403"),
+					@ApiResponse(description = "Not Found", responseCode = "404"),
+					@ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+			})
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@PutMapping(produces = "application/json")
 	public MovieDTO saveScore(@Valid @RequestBody ScoreDTO dto) {
