648179372a0c678c0b03963ba5b80ba555ca565c
lucasdanilox <lukastecno@gmail.com>
2024-02-08 23:35:58 -0300

    Configuration Class

---
+++ src/main/java/com/devsuperior/dsmovie/config/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsmovie.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+@OpenAPIDefinition
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI dsmovieAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DSMovie API")
+                        .description("DSMovie Reference Project")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://github.com/devsuperior/dsmovie-ref")));
+    }
+
+
+}
